88b4192e471b29ba411e106f3c059b8e4f902e83
drs317lakshami <devrajkumar317@gmail.com>
2022-08-28 11:57:07 +0530

    Adding:Add the Part time Employee Wage

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -2,16 +2,22 @@ public class EmployeeWages{
     public static void main(String args[])
     {
 
-    final int FULL_TIME = 1;
+    final int PART_TIME = 1;
+    final int FULL_TIME = 2;
     final int Wage_Per_Hr = 20;
-    int empType = (int) (Math.random() * 100) % 2;
+    int empType = (int) (Math.random() * 100) % 3;
     int workingHours = 0;
         if (empType == FULL_TIME)
     {
-        System.out.println("Employee is Present");
+        System.out.println("Employee is Present Full Time");
         workingHours = 8;
     }
-        else
+        else if (empType == PART_TIME)
+        {
+            System.out.println("Employee is Present Part Time");
+            workingHours = 4;
+        }
+        else	
     {
         System.out.println("Employee is Absent");
     }
