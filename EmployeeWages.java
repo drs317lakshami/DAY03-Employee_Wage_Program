3055a05a9c8fe85fc67c9c19295e3a2b77796a37
drs317lakshami <devrajkumar317@gmail.com>
2022-08-28 11:38:45 +0530

    Adding:Employee is Present Or Absent

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -1,5 +1,15 @@
 public class EmployeeWages {
-	public static void main(String[] args) {
-	     System.out.println("Welcome to Employee Wage Computation Program");
-		}
-	}
+    public static void main(String args[])
+    {
+
+    final int FULL_TIME = 1;
+    int empType = (int) (Math.random() * 100) % 2;
+        if (empType == FULL_TIME)
+    {
+        System.out.println("Employee is Present");
+    } else
+    {
+        System.out.println("Employee is Absent");
+    }
+}
+}
